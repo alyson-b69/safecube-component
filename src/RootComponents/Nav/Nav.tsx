33c12a29b129e38bb9c8116c3f643be43129ca00
alyson-b69 <alyson.bernabeu@gmail.com>
2021-04-21 20:29:11 +0200

    try hashrouter

--- src/RootComponents/Nav/Nav.tsx
+++ src/RootComponents/Nav/Nav.tsx
@@ -1,18 +1,20 @@
-import { StyledNav } from "./Nav.style"
-const url = process.env.PUBLIC_URL;
+import { StyledNav } from "./Nav.style";
+import {Link} from 'react-router-dom';
+
 const Nav = () => {
     return (
-    <StyledNav>
-        <p data-item='SAFECUBE'>SAFECUBE</p>
-        <ul className="menuItems">
-            <li><a href={'/'} data-item='Home'>Home</a></li>
-            <li><a href={'/liste'} data-item='Liste'>Liste</a></li>
-            <li><a href={'/charts'} data-item='Charts'>Charts</a></li>
-            <li><a href={'/chartsv2'} data-item='ChartsV2'>ChartsV2</a></li>
-            <li><a href={'/aviexp'}  data-item='aviexp'>AviExp</a></li>
-        </ul>
-    </StyledNav>
-    )
+        <StyledNav>
+            <p data-item='SAFECUBE'>SAFECUBE</p>
+            <ul className="menuItems">
+                <li><a href={'/'} data-item='Home'>Home</a></li>
+                <li><a href={'/liste'} data-item='Liste'>Liste</a></li>
+                <li><a href={'/charts'} data-item='Charts'>Charts</a></li>
+                <li><a href={'/chartsv2'} data-item='ChartsV2'>ChartsV2</a></li>
+                <li><a href={'/aviexp'} data-item='aviexp'>AviExp</a></li>
+                <Link to={'/aviexp'}>AEX</Link>
+            </ul>
+        </StyledNav>
+    );
 }
 
 export default Nav
